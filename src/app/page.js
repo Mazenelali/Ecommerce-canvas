2c190be50abaa56d25d5b3a2ab52e7967cd0c090
Mazen El Ali <mazenelali>
2023-07-12 00:41:03 +0300

    edit 3 version 1

--- src/app/page.js
+++ src/app/page.js
@@ -148,7 +148,6 @@ export default function Home() {
                     now and embark on the journey of crafting your dream home!
                     Shop with us today and unlock a world of possibilities for
                     your home decor and furniture needs.
-                    
                 </p>
             </div>
             <Footer/>
